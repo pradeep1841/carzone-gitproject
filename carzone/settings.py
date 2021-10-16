0967178f3d352985dbfe7dac92d04f87b52d00c5
Pradeep <pradeep0987.kr@gmail.com>
2021-10-16 19:18:00 +0530

    Added

--- carzone/settings.py
+++ carzone/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         # 'NAME': BASE_DIR / 'db.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'carzone_db',
+        'USER': 'postgres',
+        'PASSWORD':'postgres',
+        'HOST': 'localhost',
+
     }
 }
 
@@ -128,3 +132,5 @@ os.path.join(BASE_DIR, 'carzone/static'),
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
+MEDIA_URL='/media/'
